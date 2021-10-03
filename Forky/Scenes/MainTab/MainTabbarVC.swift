845105dad93fc683b6f3f59e5309f53bbca40c5b
sadariyavivek <95381vivek@gmail.com>
2021-10-03 19:52:25 +0530

    first commit

---
+++ Forky/Scenes/MainTab/MainTabbarVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabbarVC.swift
+//  Forky
+//
+//  Created by Vivek Sadariya on 10/09/21.
+//
+
+import UIKit
+
+class MainTabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
